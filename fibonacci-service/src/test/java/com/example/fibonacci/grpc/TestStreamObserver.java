7e9b10697b28069beb253a73f50668726bdd34ef
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-06-17 14:23:09 +0300

    feat: Create TestStreamObserver utility for gRPC testing

---
+++ fibonacci-service/src/test/java/com/example/fibonacci/grpc/TestStreamObserver.java
@@ -0,0 +1,40 @@
+package com.example.fibonacci.grpc;
+
+import io.grpc.stub.StreamObserver;
+
+public class TestStreamObserver implements StreamObserver<math.MathResponse> {
+    private math.MathResponse response;
+    private Throwable error;
+    private boolean completed = false;
+    
+    @Override
+    public void onNext(math.MathResponse value) {
+        this.response = value;
+    }
+    
+    @Override
+    public void onError(Throwable t) {
+        this.error = t;
+    }
+    
+    @Override
+    public void onCompleted() {
+        this.completed = true;
+    }
+    
+    public math.MathResponse getResponse() {
+        return response;
+    }
+    
+    public Throwable getError() {
+        return error;
+    }
+    
+    public boolean isError() {
+        return error != null;
+    }
+    
+    public boolean isCompleted() {
+        return completed;
+    }
+} 
